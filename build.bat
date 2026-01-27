374210b6272483145e9fdc3f0d405bb8373dfc95
Mairooriam <mirosanteriloisa@gmail.com>
2026-01-28 01:19:24 +0200

    strugling with building

--- build.bat
+++ build.bat
@@ -1,11 +1,8 @@
 @echo off
 if not exist build (
     mkdir build
+    cmake --preset default
 )
-cd build
-cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Debug ../src
-set CLICOLOR_FORCE=1
-ninja
 
-REM Go back to src directory
-cd ..
+set CLICOLOR_FORCE=1
+cmake --build build --config Debug
