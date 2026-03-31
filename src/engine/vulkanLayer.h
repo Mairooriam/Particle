90731e51c5089c1ef4acab67f8445cea00b80588
Miro Loisa <mirosanteriloisa@gmail.com>
2026-03-31 20:18:12 +0300

    moving towards c++

--- src/engine/vulkanLayer.h
+++ src/engine/vulkanLayer.h
@@ -1,3 +1,24 @@
 #pragma once
+#include <vulkan/vulkan.h>
+#include <memory>
+#include <vector>
+#include <SDL3/SDL.h>
 
-void render();
+struct vulkanContext {
+  VkBuffer vBuffer;
+  VkDeviceSize vBufSize = 0;
+  VkDeviceSize indexCount = 0;
+  SDL_Window *window = nullptr;
+  std::vector<VkPhysicalDevice> devices{};
+  uint32_t deviceIndex = 0;
+  VkSurfaceCapabilitiesKHR surfaceCaps{};
+  VkSwapchainKHR swapchain{VK_NULL_HANDLE};
+  VkSwapchainCreateInfoKHR swapchainCI{};
+  uint32_t imageCount = 0;
+  VkImageCreateInfo depthImageCI{};
+  VkFormat depthFormat{VK_FORMAT_UNDEFINED};
+};
+
+void init(std::unique_ptr<vulkanContext> ctx);
+void render(std::unique_ptr<vulkanContext> ctx);
+void destroy(std::unique_ptr<vulkanContext> ctx);
